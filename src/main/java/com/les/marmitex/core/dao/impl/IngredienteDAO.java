6c540160365183514118813128a62a638bc10066
Gustavo Bezerra <gustavo.bezerra@hotmail.com>
2016-08-29 16:20:34 -0300

    Criados os DAOs; Meus pedidos utilizando AngularJS

---
+++ src/main/java/com/les/marmitex/core/dao/impl/IngredienteDAO.java
@@ -0,0 +1,10 @@
+package com.les.marmitex.core.dao.impl;
+
+/**
+ * Classe responsável pelas ações na tb_ingrediente
+ * @author Gustavo de Souza Bezerra <gustavo.bezerra@hotmail.com>
+ * @date 27/08/2016
+ */
+public class IngredienteDAO {
+    
+}
